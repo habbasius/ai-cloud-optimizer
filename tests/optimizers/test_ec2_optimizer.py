45b3e36f41d3ce70ef83f1da99788bdc7029234e
Hamid Abbasi <hamid.abbasi.us@gmail.com>
2025-02-08 23:21:01 -0600

    Add initial test structure

---
+++ tests/optimizers/test_ec2_optimizer.py
@@ -0,0 +1,10 @@
+import pytest
+from src.optimizers.ec2_optimizer import EC2Optimizer
+
+def test_ec2_optimizer_initialization():
+    optimizer = EC2Optimizer()
+    assert optimizer is not None
+
+def test_ec2_optimization_analysis():
+    # Add your test cases here
+    pass
